169498c4db00926d6e83e95aabd01e44e7427757
Wiktor Sadowski <58751887+Sadowskyy@users.noreply.github.com>
2023-03-03 16:27:07 +0100

    Created new table. (#3)
    #1

--- dev/mysql/dump.sql
+++ dev/mysql/dump.sql
@@ -28,6 +28,27 @@ VALUES (1, '07153a4a-f96b-11e9-afd5-0242ac12000b', 'admin@symfony.dev', '$2a$12$
 
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `auctions`;
+
+CREATE TABLE `auctions` (
+    `id`    int(10) NOT NULL AUTO_INCREMENT,
+    `uuid`  varchar(36) CHARACTER SET utf8  NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    `start_time` datetime NOT NULL,
+    `end_time` datetime NOT NULL,
+    `starting_price` int(10) NOT NULL,
+    `current_bid` int(10) NOT NULL,
+    `seller_id` INT NOT NULL,
+    `winner_id` INT,
+    `status` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT fk_auctions_sellers FOREIGN KEY (seller_id) REFERENCES users(id),
+    CONSTRAINT fk_auctions_winners FOREIGN KEY (winner_id) REFERENCES users(id)
+);
+
 CREATE TABLE `roles`
 (
     `id`         int(10) NOT NULL AUTO_INCREMENT,
